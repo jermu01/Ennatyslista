a8fc5b4892c3dfbe3972f93a295fe2cff7e9f1ad
jermu01 <49148862+jermu01@users.noreply.github.com>
2021-01-14 19:32:44 +0200

    .

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
+        block.style.animation = "none";
         counter=0;
         block.style.animation = "block 1s infinite linear";
     }else{
