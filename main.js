d9d39da273d7532362714e4300cd613641dbb034
jermu01 <49148862+jermu01@users.noreply.github.com>
2021-01-15 09:53:57 +0200

    .

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-var img = document.createElement("character");
+var character = document.createElement("character");
 var block = document.getElementById("block");
 var counter=0;
 function jump(){
-    if(character.classList == "animate"){
-    character.classList.add("animate");
-    }
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },300);
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+}
+setTimeout(function(){
+    character.classList.remove("animate");
+    },500);
 }
 var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
         block.style.animation = "none";
         block.style.display = "none";
         counter=0;
-        block.style.animation = "block 3s infinite linear";
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
